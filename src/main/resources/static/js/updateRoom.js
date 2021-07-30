480fdbc82e3dc42b7bee97eb387fd32c5168dcac
18-xjh <535309586@qq.com>
2021-07-30 09:24:40 +0800

    界面美化

--- src/main/resources/static/js/updateRoom.js
+++ src/main/resources/static/js/updateRoom.js
@@ -16,16 +16,6 @@ function shield(a,b,c,d){
     var l = document.getElementById("log_window");
     l.style.display = "block";
 
-    // 根据点击修改自习室信息自动获取对应座位号并放入对应的框中
-    // var roomId = document.getElementById("RoomId").innerText;
-    // console.log(roomId)
-    //
-    //
-    // xmlHttp = new XMLHttpRequest();//创建request对象
-    // xmlHttp.onreadystatechange = checkReserve;  // 服务器响应后，谁负责处理服务器响应的数据
-    // xmlHttp.open("GET", "getRoomById.do?roomId=" + roomId);  // 開啟連結
-    // xmlHttp.send(null);  // 傳送請求
-
 }
 
 function cancel_shield(){
