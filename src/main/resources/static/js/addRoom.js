7949301469f73e49051e5c896818baa88c264d2f
18-xjh <535309586@qq.com>
2021-08-02 09:34:46 +0800

    修改了添加用户时的ID的异常，修改了一下错误

--- src/main/resources/static/js/addRoom.js
+++ src/main/resources/static/js/addRoom.js
@@ -11,14 +11,11 @@ xmlHttp.send(null);  // 傳送請求
 function checkReserve() {
     if (xmlHttp.readyState == 4) {
         if (xmlHttp.status == 200) {
-            // console.log(typeof xmlHttp.responseText);//服务器响应的原始数据
-            // console.log(typeof eval('('+xmlHttp.responseText+')'));//不推荐使用
-            // console.log(typeof JSON.parse(xmlHttp.responseText));
             rel = JSON.parse(xmlHttp.responseText);
             if (rel.result == "roomToYes") {
-                img.src = '/img/n.png'
-            } else if (rel.result == "roomToNo") {
                 img.src = '/img/y.png'
+            } else if (rel.result == "roomToNo") {
+                img.src = '/img/n.png'
             }
         }
     }
