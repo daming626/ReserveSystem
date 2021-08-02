81655a65b5c96fd0100e356feba2e97d6d9fb262
oujiaming <1099475902@qq.com>
2021-08-02 23:16:28 +0800

    整合

--- src/main/resources/static/js/showMenu.js
+++ src/main/resources/static/js/showMenu.js
@@ -2,10 +2,10 @@ function showMenu(ele) {
     var currentStatus = ele.nextSibling.nextSibling.style.display;
     if (currentStatus == "block") {
         ele.nextSibling.nextSibling.style.display = "none";
-        ele.children[0].src="/static/img/open.png";
+        ele.children[0].src="../img/open.png";
     }
     else {
         ele.nextSibling.nextSibling.style.display = "block";
-        ele.children[0].src="/static/img/close.png";
+        ele.children[0].src="../img/close.png";
     }
 }
